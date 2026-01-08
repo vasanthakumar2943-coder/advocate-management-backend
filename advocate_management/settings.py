36f984964a4d6b4c6faf59665ab3c351264d12c4
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 23:35:43 +0530

    fix urls.py name error

--- advocate_management/settings.py
+++ advocate_management/settings.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = [
 # APPS
 # =========================
 INSTALLED_APPS = [
-   
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
