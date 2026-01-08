591211cb3b36d82a048399f7894af284c765962e
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 08:07:29 +0530

    Fix admin static files using WhiteNoise

--- advocate_management/settings.py
+++ advocate_management/settings.py
@@ -60,16 +60,17 @@ REST_FRAMEWORK = {
 # MIDDLEWARE  (ORDER IS CRITICAL)
 # =====================================================
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",          # MUST BE FIRST
-    "django.middleware.common.CommonMiddleware",
-
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # ✅ ADD THIS
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 # =====================================================
 # CORS / CSRF  (VERCEL → RAILWAY)
@@ -141,6 +142,8 @@ USE_TZ = True
 # =====================================================
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
