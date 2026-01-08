fd6602ee8915b8a6e25e76913b99c04c7e01d7f3
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 08:22:05 +0530

    Fix admin static files for Daphne using WhiteNoise ASGI

--- advocate_management/asgi.py
+++ advocate_management/asgi.py
@@ -1,14 +1,25 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from users.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
+from whitenoise import WhiteNoise
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "advocate_management.settings"
+import users.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advocate_management.settings")
+
+django_asgi_app = get_asgi_application()
+
+# 🔥 WhiteNoise wraps ASGI app (THIS FIXES ADMIN CSS)
+django_asgi_app = WhiteNoise(
+    django_asgi_app,
+    root="/app/staticfiles",
+    prefix="static/",
 )
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(users.routing.websocket_urlpatterns)
+    ),
 })
