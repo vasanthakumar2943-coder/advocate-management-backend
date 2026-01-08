9c541c3f5cebdd6319b40ec1c9d29f163174970d
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 09:31:00 +0530

    Fix JWT WebSocket middleware stack

--- users/middleware.py
+++ users/middleware.py
@@ -1,27 +1,35 @@
 from urllib.parse import parse_qs
-from django.contrib.auth.models import AnonymousUser
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import AccessToken
 from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
+from channels.auth import AuthMiddlewareStack
+from django.contrib.auth.models import AnonymousUser
+
+User = get_user_model()
 
 @database_sync_to_async
-def get_user_from_token(token):
+def get_user(token):
     try:
-        validated = JWTAuthentication().get_validated_token(token)
-        return JWTAuthentication().get_user(validated)
+        access = AccessToken(token)
+        return User.objects.get(id=access["user_id"])
     except Exception:
         return AnonymousUser()
 
-class JWTAuthMiddleware:
-    def __init__(self, app):
-        self.app = app
-
+class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        query_string = parse_qs(scope["query_string"].decode())
-        token = query_string.get("token")
+        query_string = scope.get("query_string", b"").decode()
+        params = parse_qs(query_string)
+
+        token = params.get("token")
 
         if token:
-            scope["user"] = await get_user_from_token(token[0])
+            scope["user"] = await get_user(token[0])
         else:
             scope["user"] = AnonymousUser()
 
-        return await self.app(scope, receive, send)
+        return await super().__call__(scope, receive, send)
+
+
+def JWTAuthMiddlewareStack(inner):
+    return JWTAuthMiddleware(AuthMiddlewareStack(inner))
