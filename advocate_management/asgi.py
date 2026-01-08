e06ac7b53a64941102153b8cf2f9da874db9ea8b
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 05:43:31 +0530

    Fix Django version and requirements for Railway

--- advocate_management/asgi.py
+++ advocate_management/asgi.py
@@ -1,14 +1,14 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 from users.routing import websocket_urlpatterns
-from users.middleware import JWTAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advocate_management.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "advocate_management.settings"
+)
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        URLRouter(websocket_urlpatterns)
-    ),
+    "websocket": URLRouter(websocket_urlpatterns),
 })
