07665133af4038bf231e344022411bbfc61da320
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 11:09:28 +0530

    Remove websocket and switch to REST chat

--- advocate_management/asgi.py
+++ advocate_management/asgi.py
@@ -1,16 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from users.middleware import JWTAuthMiddleware
-import users.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advocate_management.settings")
 
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": JWTAuthMiddleware(
-        URLRouter(users.routing.websocket_urlpatterns)
-    ),
-})
+application = get_asgi_application()
--- users/urls.py
+++ users/urls.py
@@ -1,29 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import *
+from .views import signup, me, chat_messages
 
 urlpatterns = [
-    # AUTH
-    path("auth/login/", TokenObtainPairView.as_view()),
-    path("auth/refresh/", TokenRefreshView.as_view()),
     path("auth/signup/", signup),
     path("auth/me/", me),
-
-    # ADMIN
-    path("admin/pending-advocates/", pending_advocates),
-    path("admin/approve-advocate/<int:user_id>/", approve_advocate),
-
-    # CLIENT
-    path("client/advocates/", list_advocates),
-    path("client/book-appointment/", book_appointment),
-    path("client/my-appointments/", my_appointments),
-
-    # ADVOCATE
-    path("advocate/my-appointments/", my_appointments),
-    path("advocate/approve-appointment/<int:appointment_id>/", approve_appointment),
-    path("advocate/create-case/", create_case),
-    path("advocate/my-cases/", my_cases),
-
-    # CHAT
     path("chat/<int:appointment_id>/", chat_messages),
 ]
