838c790eaa95751fa67f940388f685f5fdd6285e
Vasanth <vasanthakumar2943@gmail.com>
2026-01-08 10:20:17 +0530

    Remove WhiteNoise and fix settings for Railway

--- advocate_management/asgi.py
+++ advocate_management/asgi.py
@@ -1,16 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from users.middleware import JWTAuthMiddleware
 import users.routing
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "advocate_management.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advocate_management.settings")
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(
-        users.routing.websocket_urlpatterns
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddleware(
+        URLRouter(users.routing.websocket_urlpatterns)
     ),
 })
--- users/middleware.py
+++ users/middleware.py
@@ -1,35 +1,26 @@
 from urllib.parse import parse_qs
-from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import AccessToken
-from channels.db import database_sync_to_async
-from channels.middleware import BaseMiddleware
-from channels.auth import AuthMiddlewareStack
 from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from channels.db import database_sync_to_async
 
-User = get_user_model()
+class JWTAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
 
-@database_sync_to_async
-def get_user(token):
-    try:
-        access = AccessToken(token)
-        return User.objects.get(id=access["user_id"])
-    except Exception:
-        return AnonymousUser()
-
-class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        query_string = scope.get("query_string", b"").decode()
-        params = parse_qs(query_string)
-
-        token = params.get("token")
+        query = parse_qs(scope["query_string"].decode())
+        token = query.get("token")
 
         if token:
-            scope["user"] = await get_user(token[0])
+            user = await self.get_user(token[0])
+            scope["user"] = user
         else:
             scope["user"] = AnonymousUser()
 
-        return await super().__call__(scope, receive, send)
+        return await self.inner(scope, receive, send)
 
-
-def JWTAuthMiddlewareStack(inner):
-    return JWTAuthMiddleware(AuthMiddlewareStack(inner))
+    @database_sync_to_async
+    def get_user(self, token):
+        jwt_auth = JWTAuthentication()
+        validated = jwt_auth.get_validated_token(token)
+        return jwt_auth.get_user(validated)
