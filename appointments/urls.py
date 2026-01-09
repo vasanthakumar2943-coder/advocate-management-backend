e3f0a80c956ab37529582514648fc251f613c17e
Vasanth <vasanthakumar2943@gmail.com>
2026-01-09 17:56:20 +0530

    fix csrf trusted origins for admin

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import create_appointment, advocate_requests, approve_appointment
+from django.http import HttpResponse
+
+def test_api(request):
+    return HttpResponse("USERS URL WORKING")
+
 
 urlpatterns = [
     path("appointments/", create_appointment),
     path("appointments/requests/", advocate_requests),
     path("appointments/approve/<int:pk>/", approve_appointment),
+    path("test/", test_api),
+
 ]
