e0e4df3b927c41d6dbca7c1c7a400b1719d342ae
Vasanth <vasanthakumar2943@gmail.com>
2026-01-11 14:29:51 +0530

    fix Appointment model user FK

--- appointments/apps.py
+++ appointments/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class AppointmentsConfig(AppConfig):
-    name = 'appointments'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appointments"
